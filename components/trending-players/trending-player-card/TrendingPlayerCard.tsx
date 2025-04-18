ff0eb5bf2d1e677271c5448ecea3ad9b7d2a632c
jDelille <justind7513@gmail.com>
2025-04-18 12:05:42 -0700

    chore: added sass package

---
+++ components/trending-players/trending-player-card/TrendingPlayerCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type TrendingPlayerCardProps = {
+ 
+ }
+const TrendingPlayerCard: React.FC<TrendingPlayerCardProps> = () => {
+  return (
+    <div className="trending-player-card">
+      
+    </div>
+  );
+};
+
+export default TrendingPlayerCard;
